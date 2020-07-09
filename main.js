b9fbf91448e806f0163b1ea3faef1d3e3871957e
crazould <crazould@gmail.com>
2020-07-09 18:32:12 +0700

    add some changes

--- main.js
+++ main.js
@@ -6,12 +6,30 @@ let taskList = []
 
 taskBtn.addEventListener("click", (e) => {
 
-    let task = '<div class= "task">' + taskTxt.value + '</div>'
+    if (taskTxt.value == "") {
+        // taskBtn.style.boxShadow = "none"
+        taskTxt.style.boxShadow = "0px 0px 15px #a31d1d"
+        taskTxt.placeholder = "Type your todo here!"
+        console.log(taskBtn.style.boxShadow)
+    }
+    else {
 
+        let task = 
+        `<div class= "task"> 
+            <div class="task-text">
+                ${taskTxt.value} 
+            </div>
+            <div class="task-action">
+                <i class="fa fa-check fa-lg my-btn" id="editBtn" aria-hidden="true"></i>
+                <i class="fa fa-trash fa-lg my-btn" id="deleteBtn" aria-hidden="true"></i>
+            </div>
+        </div>`
 
-    taskList.push(task)
+        taskList.push(task)
+        tasks.innerHTML = taskList.join(" ")
+	taskTxt.value = ""
 
-    tasks.innerHTML = taskList.join(" ")
+    }
 
 
 })
