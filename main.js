9136062b7d14a90bc68d1a7151d1e8df7bd1739b
crazould <crazould@gmail.com>
2020-07-25 11:58:19 +0700

    complete all fitures

--- main.js
+++ main.js
@@ -1,35 +1,101 @@
-let taskBtn = document.getElementById("taskBtn")
+let addBtn = document.getElementById("addBtn")
 let taskTxt = document.getElementById("taskTxt")
-let tasks = document.getElementById("tasklist")
-let taskList = []
+let tasksList = document.getElementById("tasklist")
 
-
-taskBtn.addEventListener("click", (e) => {
+let addTask = () => {
 
     if (taskTxt.value == "") {
-        // taskBtn.style.boxShadow = "none"
+        
         taskTxt.style.boxShadow = "0px 0px 15px #a31d1d"
         taskTxt.placeholder = "Type your todo here!"
-        console.log(taskBtn.style.boxShadow)
+
     }
     else {
 
-        let task = 
-        `<div class= "task"> 
-            <div class="task-text">
-                ${taskTxt.value} 
-            </div>
-            <div class="task-action">
-                <i class="fa fa-check fa-lg my-btn" id="editBtn" aria-hidden="true"></i>
-                <i class="fa fa-trash fa-lg my-btn" id="deleteBtn" aria-hidden="true"></i>
-            </div>
-        </div>`
+        let myTask = document.createElement("div")
+        let myTaskText = document.createElement("div")
+        let myTaskAction = document.createElement("div")
+        let myDoneIcon = document.createElement("i")
+        let myDeleteIcon = document.createElement("i")
 
-        taskList.push(task)
-        tasks.innerHTML = taskList.join(" ")
-	taskTxt.value = ""
+        myTask.classList.add("task")
+        myTaskText.classList.add("task-text")
+        myTaskAction.classList.add("task-action")
 
+        myDoneIcon.classList.add("fa")
+        myDoneIcon.classList.add("fa-check")
+        myDoneIcon.classList.add("fa-lg")
+        myDoneIcon.classList.add("my-btn")
+        myDoneIcon.classList.add("done-btn")
+
+        myDeleteIcon.classList.add("fa")
+        myDeleteIcon.classList.add("fa-trash")
+        myDeleteIcon.classList.add("fa-lg")
+        myDeleteIcon.classList.add("my-btn")
+        myDeleteIcon.classList.add("delete-btn")
+
+        myTaskText.innerText = taskTxt.value
+        
+        myDoneIcon.addEventListener('click', (e) => {
+            
+            myTaskText.style.textDecoration = "line-through"
+            // myTask.style.backgroundColor = "#f33e59"
+            // myDoneIcon.style.color = "#20CC82"
+            myTask.style.backgroundColor = "#f33e59"
+
+        })
+        
+        myDeleteIcon.addEventListener('click', (e) => {
+            let position = 0
+            opacity = 1
+            myTask.style.opacity = opacity
+
+            let fadeOut = setInterval(() => {
+
+                if (position == 400) {
+                    tasksList.removeChild(myTask)
+                    clearInterval(fadeOut)
+                } else {
+                    // console.log(fadeInOpacity)
+                    myTask.style.left = position + 'px'
+                    myTask.style.opacity = opacity
+                    position = position + 25
+                    opacity = opacity - 0.1
+                } 
+
+            }, 50)
+
+        })
+
+        myTaskAction.appendChild(myDoneIcon)
+        myTaskAction.appendChild(myDeleteIcon)
+
+        myTask.appendChild(myTaskText);
+        myTask.appendChild(myTaskAction);
+
+        tasksList.prepend(myTask)
+
+        let opacity = 0;
+        myTask.style.opacity = opacity;
+
+        let fadeIn = setInterval(() => {
+            if (opacity == 0.9999999999999999) {
+                clearInterval(fadeIn)
+            } else {
+                myTask.style.opacity = opacity
+                opacity = opacity + 0.1
+            } 
+        }, 25)
+
+        taskTxt.value = ""
     }
 
+}
 
+addBtn.addEventListener("click", () => addTask())
+document.addEventListener("keydown", (e) => {
+    if (e.keyCode == 13) addTask()
 })
+
+
+
