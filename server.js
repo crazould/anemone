de15c070784d4d781cc1fa24b82599b2eb0ad23c
crazould <crazould@gmail.com>
2020-07-26 10:44:18 +0700

    fix responsive

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 const port = process.env.PORT || 3000
-
 app.use(express.static(__dirname))
 
 router.get('/',(req,res) => res.sendFile(`${__dirname}/index.html`))
 
 app.use( '/' ,router);
-
 app.listen(port, () => console.log(`app running on port: ${port}`))
